e4a368ab8042f1c4518b35b249391ac7a65f9288
Tate Keller <tatek1993@gmail.com>
2020-02-14 13:09:51 -0600

    Created test in App.test.js to make sure the Navigation bar is rendering using getByText and toBeInDocument, test passed

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { render, toBeInTheDocument } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 import ReactDOM from 'react-dom';
 import App from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('Nav title renders', () => {
+  const { getByText } = render(<App />);
+
+  const navTitle = getByText(/Meet the Players/i);
+  expect(navTitle).toBeInTheDocument();
+});
+
+
+
